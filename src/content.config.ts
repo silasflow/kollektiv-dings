dfc5709e15f9c6b1177c3cead8dce2ad17071051
Lysann Herms <lysannnora@googlemail.com>
2026-06-29 22:05:48 +0200

    blog Dinger aus templet gelöscht

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,22 +2,7 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-			// Optional language identifier for the post
-			lang: z.enum(['en', 'de']).optional(),
-		}),
-});
+
 
 const pages = defineCollection({
 	// Load Markdown and MDX files in the `src/content/pages/` directory.
@@ -35,4 +20,4 @@ const pages = defineCollection({
 		}),
 });
 
-export const collections = { blog, pages };
+export const collections = { pages };
