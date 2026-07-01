5c287ab1865d7404b94fa06bb705727d40150f86
silasflow <93256160+silasflow@users.noreply.github.com>
2026-07-01 17:03:58 +0200

    uninstall netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
-
 export default defineConfig({
   site: 'https://stadt-kollektiv.de',
   integrations: [react(), mdx(), sitemap()],
 
   // This is the default, but explicit is better
   output: 'static',
-
-  adapter: netlify()
 });
