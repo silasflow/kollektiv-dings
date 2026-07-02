5776162dc83bbefdd83ae4b1d550b45581de4e83
Lysann Herms <lysannnora@googlemail.com>
2026-07-02 14:51:08 +0200

    node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://stadt-kollektiv.de',
   integrations: [react(), mdx(), sitemap()],
 
-  // This is the default, but explicit is better
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
