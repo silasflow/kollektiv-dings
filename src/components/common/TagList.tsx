d4592c02a65c996c07a458970d011c68b03b0f77
Lysann Herms <lysannnora@googlemail.com>
2026-07-08 18:17:49 +0200

    feat: add TagList component for displaying a list of tags with styles

---
+++ src/components/common/TagList.tsx
@@ -0,0 +1,18 @@
+type TagListProps = {
+  items: string[];
+  className?: string;
+};
+
+export default function TagList({ items, className = '' }: TagListProps) {
+  if (items.length === 0) return null;
+
+  return (
+    <ul className={`tag-list ${className}`}>
+      {items.map((item) => (
+        <li className="tag-list__item" key={item}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+}
